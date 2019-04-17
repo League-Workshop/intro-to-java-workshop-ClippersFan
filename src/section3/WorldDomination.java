1596481d98e4f7fce943ea57d3c90a1998772545
ClippersFan <league@WTS5.attlocal.net>
2019-04-17 14:22:08 -0700

    Day 3 : sunglasses :

--- src/section3/WorldDomination.java
+++ src/section3/WorldDomination.java
@@ -6,11 +6,16 @@ public class WorldDomination {
 	
 	public static void main(String[] args) {
 		// 1. Ask the user if they know how to write code.
-
+	String answer = JOptionPane.showInputDialog("Do you know how to write code?");
 		// 2. If they say "yes", tell them they will rule the world.
-
+	 if(answer.equalsIgnoreCase("yes"))
+	 { String lol = JOptionPane.showInputDialog("You will rule the world!");
+		 
+	 }else {if(answer.equalsIgnoreCase("no"))
+	 { String xd = JOptionPane.showInputDialog("Good luck washing dishes!");
 		// 3. Otherwise, wish them good luck washing dishes.
 
 	}
 }
-
+	}
+}
